c29962306858ee37f6bd836fea0fff738653a70a
Ahnsj828 <ala828@naver.com>
2024-05-28 22:58:26 +0900

    240528

--- 1_Navigation/navigation01/script.js
+++ 1_Navigation/navigation01/script.js
@@ -1,7 +1,5 @@
 const trigger = document.querySelector(".trigger");
 const gnb = document.querySelector(".gnb-list");
-// const header = document.querySelector("header");
-
 
 trigger.addEventListener("click", () => {
   gnb.classList.toggle("active");
